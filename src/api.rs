77d2e6769b8fedfad0526c28439eba9b6ba7fff9
DanSnow <dododavid006@gmail.com>
2022-07-02 12:06:57 +0800

    feat: log API error

--- src/api.rs
+++ src/api.rs
@@ -1,10 +1,20 @@
 use color_eyre::Result;
+use reqwest::StatusCode;
+use tap::prelude::*;
+use tracing::error;
 
 use crate::model::ModFile;
 
 pub async fn get_mod_files(id: u32) -> Result<Vec<ModFile>> {
-    let val = reqwest::get(format!("https://addons-ecs.forgesvc.net/api/v2/addon/{}/files", id))
+    let url = format!("https://addons-ecs.forgesvc.net/api/v2/addon/{}/files", id);
+    let val = reqwest::get(url)
         .await?
+        .tap(|response| match response.status() {
+            StatusCode::OK => (),
+            code => {
+                error!("unexpected response code {} when fetching mod id {}", code, id);
+            }
+        })
         .json::<Vec<ModFile>>()
         .await?;
     Ok(val)
